f78b0850c0ae199c908ac8e6d7b1e1e9b5544bcc
Mikias Solomon Berhane <mberhane@ucsc.edu>
2023-10-29 16:00:47 -0700

    Initial commit testing.sh

--- asgn2/coolness/testing.sh
+++ asgn2/coolness/testing.sh
@@ -1,1 +1,56 @@
+#!/bin/bash
+
+# Zero argument
+./coolness > output.txt
+diff -q output.txt noInputs.txt
+if [ $? -eq 0 ]; then
+  echo "Successful of Matching"
+else
+  echo "Unsuccessful of Matching"
+fi
+
+# Only T
+./coolness 32.0 > output.txt
+diff -q output.txt oneInput.txt
+if [ $? -eq 0 ]; then
+  echo "Successful of Matching"
+else
+  echo "Unsuccessful of Matching"
+fi
+
+# Both T and V provided
+./coolness 32.5 10.0 > output.txt
+diff -q output.txt twoInput.txt
+if [ $? -eq 0 ]; then
+  echo "Successful of Matching"
+else
+  echo "Unsuccessful of Matching"
+fi
+
+# Passed the capstone of TEMP
+# It tests if an error occurs and if it does, it will say succesful
+./coolness 116.0 10.0
+if [ $? -eq 1 ]; then
+  echo "Returned an Error: Successful"
+else
+  echo "Failed: Unsuccessful in returning Error"
+fi
+
+# Didn't surpass the wind limit
+./coolness 25.0 0.2
+if [ $? -eq 1 ]; then
+  echo "Returned an Error: Successful"
+else
+  echo "Failed: Unsuccessful in returning Error"
+fi
+
+# More arguments provided
+./coolness 25.0 10.0 15.0
+if [ $? -eq 1 ]; then
+  echo "Returned an Error: Successful"
+else
+  echo "Failed: Unsuccessful in returning Error"
+fi
+
+rm -f output.txt
 
