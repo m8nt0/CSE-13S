24fc5db275906adcd1f971a11cc5e4b596b186ee
Mikias Solomon Berhane <mberhane@ucsc.edu>
2023-12-09 20:57:35 -0800

    Replace crawler.h

--- asgn6/crawler.h
+++ asgn6/crawler.h
@@ -1,1 +1,31 @@
+#ifndef CRAWLER_H
+#define CRAWLER_H
+
+#include <stddef.h>
+#include <stdbool.h>
+//++++++++++
+//#include "bag.h"
+
+typedef struct {
+    char *the_url;
+    char *html;
+    size_t length;
+    int depth;
+} webpage_t;
+
+// Basic implementation of bag data structure using an array
+typedef struct {
+    webpage_t **pages_hold;
+    size_t limit;
+    size_t range;
+} bag_t;
+
+// Function prototypes for bag.c
+bag_t *bag_to_create(size_t limit);
+bool bag_insert(bag_t *bag, webpage_t *page);
+bool is_bag_empty(const bag_t *bag);
+webpage_t *bag_extract(bag_t *bag);
+void bag_destroy(bag_t *bag);
+
+#endif
 
