01ff9fe2210a3733ce3b3743d77eb83450935e39
Mikias Solomon Berhane <mberhane@ucsc.edu>
2023-12-09 21:08:17 -0800

    Replace set.h

--- asgn6/set.h
+++ asgn6/set.h
@@ -1,1 +1,53 @@
+#ifndef SET_H_
+#define SET_H_
 
+#include <stdbool.h> // needed to work with bool type
+#include <stdio.h>
+
+// Struct for node of key and item
+typedef struct node {
+    char *key;
+    void *item;
+    struct node *next;
+} node_pointer;
+
+// Use struct set for a way to manipulate the leading head 
+typedef struct set {
+    node_pointer *head;
+} set_t;
+
+/* Create a new (empty) set; return NULL if error. */
+set_t* set_new(void);
+
+/* Insert item, identified by a key (string), into the given set.
+ * The key string is copied for use by the set.
+ * Return false if key exists, any parameter is NULL, or error;
+ * return true iff new item was inserted.
+ */
+bool set_insert(set_t *set, const char *key, void *item);
+
+/* Return the item associated with the given key;
+ * return NULL if set is NULL, key is NULL, or key is not found.
+ */
+void *set_find(set_t *set, const char *key);
+
+/* Print the whole set; provide the output file and func to print each item.
+ * Ignore if NULL fp. Print (null) if NULL set.
+ * Print a set with no items if NULL itemprint.
+*/
+void set_print(set_t *set, FILE *fp,
+               void (*itemprint)(FILE *fp, const char *key, void *item) );
+
+/* Iterate over all items in the set, in undefined order.
+ * Call the given function on each item, with (arg, key, item).
+ * If set==NULL or itemfunc==NULL, do nothing.
+ */
+void set_iterate(set_t *set, void *arg,
+                 void (*itemfunc)(void *arg, const char *key, void *item) );
+
+/* Delete the whole set; ignore NULL set.
+ * Provide a function that will delete each item (may be NULL).
+ */
+void set_delete(set_t *set, void (*itemdelete)(void *item) );
+
+#endif // SET_H_
