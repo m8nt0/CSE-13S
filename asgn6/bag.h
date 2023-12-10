9061ce67ea115305432c7031e3ed585e770380ab
Mikias Solomon Berhane <mberhane@ucsc.edu>
2023-12-09 20:55:18 -0800

    Replace bag.h

--- asgn6/bag.h
+++ asgn6/bag.h
@@ -1,1 +1,15 @@
+#ifndef BAG_H_
+#define BAG_H_
+
+// Structure for bag ADT
+typedef struct bag bag_t;
+
+// Function prototypes
+bag_t *create_bag(int initial_capacity);
+void bag_insert(bag_t *bag, void *item);
+void *bag_extract(bag_t *bag);
+bool bag_is_empty(bag_t *bag);
+void destroy_bag(bag_t *bag);
+
+#endif // BAG_H_
 
